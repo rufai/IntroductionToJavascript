e26dde42fc05ff7db08633ba70c78e84281011ed
Rufai Mustapha <etop94@gmail.com>
2021-05-09 18:51:48 +0100

    implementing random color generation

--- colors.js
+++ colors.js
@@ -3,21 +3,32 @@ buttonElement.addEventListener("click", (e) => {
     setInterval( () => { 
         let bodyElement = document.body
         bodyElement.style.color = generateRandomColor()
+        // bodyElement.style.color = generateColorRGB()
         bodyElement.style.backgroundColor = generateRandomColor()
-    }, 10000 )
+        // bodyElement.style.backgroundColor = generateColorRGB()
+    }, 2000 )
 })
 
 function generateRandomColor() {
     let hexadecimal = [ 0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F' ]
     let color = "#"
 
-    for( let i = 0; i < 6; i += 2){
+    for( let i = 0; i < 6; i += 1){
         let element = Math.floor(Math.random() * hexadecimal.length)
         color += hexadecimal[element]
     }
+
     console.log(color)
     return color // explicit returning
 }
 
+
+function generateColorRGB() {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+
+    return rgb(r,g,b)
+}
 
 // white 
